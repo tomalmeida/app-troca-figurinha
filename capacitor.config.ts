fdaa8a614fa430934b98742670c68cc4c93d9b63
tomalmeida <tomalmeida@outook.com>
2022-06-10 17:56:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-troca-figurinha',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
